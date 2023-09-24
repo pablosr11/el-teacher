a4a7d20756544f317b786ca728ef378017ee44be
ps <pablosr11@gmail.com>
2023-09-24 08:08:48 +0100

    minimal discord bot

---
+++ disc.py
@@ -0,0 +1,17 @@
+import discord
+import httpx
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message:discord.Message):
+    if message.author == client.user:
+        return
+
